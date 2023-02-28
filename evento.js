c8cb2550ef1940cd859b6d66db610c136c0bb8ac
sebairi <126201668+sebairi@users.noreply.github.com>
2023-02-28 16:57:30 -0300

    Add files via upload

---
+++ evento.js
@@ -0,0 +1,25 @@
+
+
+function mostrarNumero() {
+    document.getElementById('numero').style.display = 'block';
+}
+
+function ocultarNumero() {
+    document.getElementById('numero').style.display = 'none';
+}
+
+function mostrarEmail() {
+    document.getElementById('email').style.display = 'block';
+}
+
+function ocultarEmail() {
+    document.getElementById('email').style.display = 'none';
+}
+
+function mostrarDireccion() {
+    document.getElementById('direccion').style.display = 'block';
+}
+
+function ocultarDireccion() {
+    document.getElementById('direccion').style.display = 'none';
+}
